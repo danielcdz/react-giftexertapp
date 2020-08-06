1d8a7e6110286eb64ef67e4ff85a5ac678c92551
Daniel <calde0601@gmail.com>
2020-08-05 23:21:13 -0600

    agregando docs

---
+++ docs/precache-manifest.a4a5989a1b2d2db30e3db98bc2fa6d49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58bf8a0d162d92da8e3e8be7e985bc2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2223f670a28aa6243e71",
+    "url": "/static/css/main.b52aecc1.chunk.css"
+  },
+  {
+    "revision": "f282757b0c05702f74d8",
+    "url": "/static/js/2.b1f59dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1f59dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2223f670a28aa6243e71",
+    "url": "/static/js/main.0505ebf8.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
